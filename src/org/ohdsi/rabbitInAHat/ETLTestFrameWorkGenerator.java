a46aab19009d61e9853ac7b3b8f920f5371ea8c7
Chris Knoll <cknoll@ohdsi.org>
2016-06-08 11:37:31 -0400

    Altered R generate functions: - createTestFramework() replaces initTestFramework: returns a 'test framework context' that encapsulates add_, declare, and generated SQL statements. - Use new.env() to store mutable objects.

--- src/org/ohdsi/rabbitInAHat/ETLTestFrameWorkGenerator.java
+++ src/org/ohdsi/rabbitInAHat/ETLTestFrameWorkGenerator.java
@@ -64,27 +64,25 @@ public class ETLTestFrameWorkGenerator {
 		out.close();
 	}
 
+	private static String convertToRName(String name) {
+		name = name.replaceAll(" ", "_").replaceAll("-", "_");
+		return name;
+	}
+	
 	private static List<String> generateRScript(ETL etl) {
 		List<String> r = new ArrayList<String>();
-		createInitFunction(r, etl.getSourceDatabase());
-		createDeclareTestFunction(r);
-		createSetDefaultFunctions(r, etl.getSourceDatabase());
-		createGetDefaultFunctions(r, etl.getSourceDatabase());
-		createAddFunctions(r, etl.getSourceDatabase());
-		createExpectFunctions(r, DEFAULT, etl.getTargetDatabase());
-		createExpectFunctions(r, NEGATE, etl.getTargetDatabase());
-		createExpectFunctions(r, COUNT, etl.getTargetDatabase());
+		createFactoryFunction(r, etl);
 		return r;
 	}
 
 	private static void createDeclareTestFunction(List<String> r) {
-		r.add("declareTest <- function(id, description) {");
-		r.add("  assign(\"testId\", id, envir = globalenv()) ");
-		r.add("  assign(\"testDescription\", description, envir = globalenv()) ");
-		r.add("  sql <- c(\"\", paste0(\"-- \", id, \": \", description))");
-		r.add("  assign(\"insertSql\", c(get(\"insertSql\", envir = globalenv()), sql), envir = globalenv())");
-		r.add("  assign(\"testSql\", c(get(\"testSql\", envir = globalenv()), sql), envir = globalenv())");
-		r.add("}");
+		r.add("  self$declareTest <- function(id, description) {");
+		r.add("    self$testId <- id;");
+		r.add("    self$testDescription <- description;");
+		r.add("    sql <- c(\"\", paste0(\"-- \", id, \": \", description))");
+		r.add("    self$insertSql <- c(self$insertSql, sql);");
+		r.add("    self$testSql <- c(self$testSql, sql);");
+		r.add("  }");
 		r.add("");
 	}
 
@@ -99,54 +97,53 @@ public class ETLTestFrameWorkGenerator {
 				String rFieldName = convertToRName(field.getName());
 				String sqlFieldName = convertToSqlName(field.getName());
 				argDefs.add(rFieldName);
-				testDefs.add("  if (!missing(" + rFieldName + ")) {");
-				testDefs.add("    if (first) {");
-				testDefs.add("      first <- FALSE");
-				testDefs.add("    } else {");
-				testDefs.add("      statement <- paste0(statement, \" AND\")");
+				testDefs.add("    if (!missing(" + rFieldName + ")) {");
+				testDefs.add("      if (first) {");
+				testDefs.add("        first <- FALSE;");
+				testDefs.add("      } else {");
+				testDefs.add("        statement <- paste0(statement, \" AND\");");
+				testDefs.add("      }");
+				testDefs.add("      if (is.null(" + rFieldName + ")) {");
+				testDefs.add("        statement <- paste0(statement, \" " + sqlFieldName + " IS NULL\");");
+				testDefs.add("      } else {");
+				testDefs.add("        statement <- paste0(statement, \" " + sqlFieldName + " = '\", " + rFieldName + ",\"'\");");
+				testDefs.add("      }");
 				testDefs.add("    }");
-				testDefs.add("    if (is.null(" + rFieldName + ")) {");
-				testDefs.add("      statement <- paste0(statement, \" " + sqlFieldName + " IS NULL\")");
-				testDefs.add("    } else {");
-				testDefs.add("      statement <- paste0(statement, \" " + sqlFieldName + " = '\", " + rFieldName + ",\"'\")");
-				testDefs.add("    }");
-				testDefs.add("  }");
 				testDefs.add("");
 			}
 
 			if (type == DEFAULT) 
-				line.append("expect_" + rTableName + " <- function(");
+				line.append("  self$expect_" + rTableName + " <- function(");
 			else if (type == NEGATE)
-				line.append("expect_no_" + rTableName + " <- function(");
+				line.append("  self$expect_no_" + rTableName + " <- function(");
 			else
-				line.append("expect_count_" + rTableName + " <- function(rowCount, ");
+				line.append("  self$expect_count_" + rTableName + " <- function(rowCount, ");
 
 			line.append(StringUtilities.join(argDefs, ", "));
 			line.append(") {");
 			r.add(line.toString());
 
 			line = new StringBuilder();
-			line.append("  statement <- paste0(\"INSERT INTO test_results SELECT ");
+			line.append("    statement <- paste0(\"INSERT INTO test_results SELECT ");
 			line.append("\", get(\"testId\", envir = globalenv()), \" AS id, ");
 			line.append("'\", get(\"testDescription\", envir = globalenv()), \"' AS description, ");
 			line.append("'Expect " + table.getName() + "' AS test, ");
 			line.append("CASE WHEN(SELECT COUNT(*) FROM " + sqlTableName + " WHERE\")");
 			r.add(line.toString());
 
-			r.add("  first <- TRUE");
+			r.add("    first <- TRUE");
 
 			r.addAll(testDefs);
 
 			if (type == DEFAULT) 
-				r.add("  statement <- paste0(statement, \") = 0 THEN 'FAIL' ELSE 'PASS' END AS status;\")");
+				r.add("    statement <- paste0(statement, \") = 0 THEN 'FAIL' ELSE 'PASS' END AS status;\")");
 			else if (type == NEGATE)
-				r.add("  statement <- paste0(statement, \") != 0 THEN 'FAIL' ELSE 'PASS' END AS status;\")");
+				r.add("    statement <- paste0(statement, \") != 0 THEN 'FAIL' ELSE 'PASS' END AS status;\")");
 			else
-				r.add("  statement <- paste0(statement, \") != \",rowCount ,\" THEN 'FAIL' ELSE 'PASS' END AS status;\")");
+				r.add("    statement <- paste0(statement, \") != \",rowCount ,\" THEN 'FAIL' ELSE 'PASS' END AS status;\")");
 
-			r.add("  assign(\"testSql\", c(get(\"testSql\", envir = globalenv()), statement), envir = globalenv())");
-			r.add("  invisible(statement)");
-			r.add("}");
+			r.add("    self$testSql <- c(self$testSql, statement);");
+			r.add("  }");
 			r.add("");
 		}
 	}
@@ -157,49 +154,6 @@ public class ETLTestFrameWorkGenerator {
 		return name;
 	}
 
-	private static void createInitFunction(List<String> r, Database database) {
-		r.add("initFramework <- function() {");
-		r.add("  insertSql <- c()");
-		for (Table table : database.getTables()) {
-			String sqlTableName = convertToSqlName(table.getName());
-			r.add("  insertSql <- c(insertSql, \"TRUNCATE TABLE " + sqlTableName + ";\")");
-		}
-		r.add("  assign(\"insertSql\", insertSql, envir = globalenv())");
-
-		r.add("  testSql <- c()");
-		r.add("  testSql <- c(testSql, \"IF OBJECT_ID('test_results', 'U') IS NOT NULL\")");
-		r.add("  testSql <- c(testSql, \"  DROP TABLE test_results;\")");
-		r.add("  testSql <- c(testSql, \"\")");
-		r.add("  testSql <- c(testSql, \"CREATE TABLE test_results (id INT, description VARCHAR(512), test VARCHAR(256), status VARCHAR(5));\")");
-		r.add("  testSql <- c(testSql, \"\")");
-
-		r.add("  assign(\"testSql\", testSql, envir = globalenv())");
-		r.add("  assign(\"testId\", 1, envir = globalenv())");
-		r.add("  assign(\"testDescription\", \"\", envir = globalenv())");
-		r.add("");
-		r.add("  defaultValues <- new.env(parent = globalenv())");
-		r.add("  assign(\"defaultValues\", defaultValues, envir = globalenv())");
-		for (Table table : database.getTables()) {
-			String rTableName = convertToRName(table.getName());
-			r.add("");
-			r.add("  defaults <- list()");
-			for (Field field : table.getFields()) {
-				String rFieldName = field.getName().replaceAll(" ", "_").replaceAll("-", "_");
-				String defaultValue;
-				if (field.getValueCounts().length == 0)
-					defaultValue = "";
-				else
-					defaultValue = field.getValueCounts()[0][0];
-				if (!defaultValue.equals(""))
-					r.add("  defaults$" + rFieldName + " <- \"" + defaultValue + "\"");
-			}
-			r.add("  assign(\"" + rTableName + "\", defaults, envir = defaultValues)");
-		}
-		r.add("}");
-		r.add("");
-		r.add("initFramework()");
-		r.add("");
-	}
 
 	private static void createAddFunctions(List<String> r, Database database) {
 		for (Table table : database.getTables()) {
@@ -212,36 +166,35 @@ public class ETLTestFrameWorkGenerator {
 				String rFieldName = field.getName().replaceAll(" ", "_").replaceAll("-", "_");
 				String sqlFieldName = convertToSqlName(field.getName());
 				argDefs.add(rFieldName);
-				insertLines.add("  if (missing(" + rFieldName + ")) {");
-				insertLines.add("    " + rFieldName + " <- defaults$" + rFieldName);
-				insertLines.add("  }");
-				insertLines.add("  if (!is.null(" + rFieldName + ")) {");
-				insertLines.add("    insertFields <- c(insertFields, \"" + sqlFieldName + "\")");
-				insertLines.add("    insertValues <- c(insertValues, " + rFieldName + ")");
-				insertLines.add("  }");
+				insertLines.add("    if (missing(" + rFieldName + ")) {");
+				insertLines.add("      " + rFieldName + " <- defaults$" + rFieldName);
+				insertLines.add("    }");
+				insertLines.add("    if (!is.null(" + rFieldName + ")) {");
+				insertLines.add("      insertFields <- c(insertFields, \"" + sqlFieldName + "\")");
+				insertLines.add("      insertValues <- c(insertValues, " + rFieldName + ")");
+				insertLines.add("    }");
 				insertLines.add("");
 			}
 
-			line.append("add_" + rTableName + " <- function(");
+			line.append("  self$add_" + rTableName + " <- function(");
 			line.append(StringUtilities.join(argDefs, ", "));
 			line.append(") {");
 			r.add(line.toString());
-			r.add("  defaults <- get(\"" + rTableName + "\", envir = defaultValues)");
-			r.add("  insertFields <- c()");
-			r.add("  insertValues <- c()");
+			r.add("    defaults <- self$defaultValues$" + rTableName + ";");
+			r.add("    insertFields <- c();");
+			r.add("    insertValues <- c();");
 			r.addAll(insertLines);
 
 			line = new StringBuilder();
-			line.append("  statement <- paste0(\"INSERT INTO " + sqlTableName + " (\", ");
+			line.append("    statement <- paste0(\"INSERT INTO " + sqlTableName + " (\", ");
 			line.append("paste(insertFields, collapse = \", \"), ");
 			line.append("\") VALUES ('\", ");
 			line.append("paste(insertValues, collapse = \"', '\"), ");
-			line.append("\"');\")");
+			line.append("\"')\");");
 			r.add(line.toString());
 
-			r.add("  assign(\"insertSql\", c(get(\"insertSql\", envir = globalenv()), statement), envir = globalenv())");
-			r.add("  invisible(statement)");
-			r.add("}");
+			r.add("    self$insertSql <- c(self$insertSql, statement);");
+			r.add("  }");
 			r.add("");
 		}
 	}
@@ -255,23 +208,19 @@ public class ETLTestFrameWorkGenerator {
 			for (Field field : table.getFields()) {
 				String rFieldName = field.getName().replaceAll(" ", "_").replaceAll("-", "_");
 				argDefs.add(rFieldName);
-				insertLines.add("  if (!missing(" + rFieldName + ")) {");
-				insertLines.add("    defaults$" + rFieldName + " <- " + rFieldName);
-				insertLines.add("  }");
+				insertLines.add("    if (!missing(" + rFieldName + ")) {");
+				insertLines.add("      defaults$" + rFieldName + " <- " + rFieldName + ";");
+				insertLines.add("    }");
 			}
 
-			line.append("set_defaults_" + rTableName + " <- function(");
+			line.append("  self$set_defaults_" + rTableName + " <- function(");
 			line.append(StringUtilities.join(argDefs, ", "));
 			line.append(") {");
 			r.add(line.toString());
-			r.add("  defaults <- get(\"" + rTableName + "\", envir = defaultValues)");
+			r.add("    defaults <- self$defaultValues$" + rTableName + ";");
 			r.addAll(insertLines);
-
-
-
-			r.add("  assign(\"" + rTableName + "\", defaults, envir = defaultValues)");
-			r.add("  invisible(defaults)");
-			r.add("}");
+			r.add("    self$defaultValues$" + rTableName + " <- defaults;");
+			r.add("  }");
 			r.add("");
 		}
 	}
@@ -279,16 +228,75 @@ public class ETLTestFrameWorkGenerator {
 	private static void createGetDefaultFunctions(List<String> r, Database database) {
 		for (Table table : database.getTables()) {
 			String rTableName = convertToRName(table.getName());
-			r.add("get_defaults_" + rTableName + " <- function() {");
-			r.add("  defaults <- get(\"" + rTableName + "\", envir = defaultValues)");
-			r.add("  return(defaults)");
-			r.add("}");
+			r.add("  self$get_defaults_" + rTableName + " <- function() {");
+			r.add("    defaults <- self$defaultValues$" + rTableName + ";");
+			r.add("    return(defaults);");
+			r.add("  }");
 			r.add("");
 		}
 	}
+	
+	private static void createFactoryFunction(List<String> r, ETL etl) {
+		Database database = etl.getSourceDatabase();
+		
+		r.add("#' Create test framework object");
+		r.add("#'");
+		r.add("#' @details");
+		r.add("#' Contains functions to condunct testing.");
+		r.add("#'");
+		r.add("#' @return");
+		r.add("#' a framework instance");
+		r.add("#'");
+		r.add("#' @export");
+		r.add("createTestFramework <- function() {");
+		r.add("  self <- new.env(parent = emptyenv());");
+		for (Table table : database.getTables()) {
+			String sqlTableName = convertToSqlName(table.getName());
+			r.add("  self$insertSql <- c(self$insertSql, \"TRUNCATE TABLE " + sqlTableName + ";\");");
+		}
 
-	private static String convertToRName(String name) {
-		name = name.replaceAll(" ", "_").replaceAll("-", "_");
-		return name;
-	}
+		r.add("  self$testSql <- c();");
+		r.add("  self$testSql <- c(self$testSql, \"IF OBJECT_ID('@testSchema.test_results', 'U') IS NOT NULL\");");
+		r.add("  self$testSql <- c(self$testSql, \"  DROP TABLE @testSchema.test_results;\");");
+		r.add("  self$testSql <- c(self$testSql, \"\");");
+		r.add("  self$testSql <- c(self$testSql, \"CREATE TABLE @testSchema.test_results (id INT, description VARCHAR(512), test VARCHAR(256), status VARCHAR(5))\");");
+		r.add("  self$testSql <- c(self$testSql, \"\");");
+		r.add("  self$testId <- 1;");
+		r.add("  self$testDescription <- \"\";");
+		r.add("");
+		r.add("  self$defaultValues <- list()");
+		for (Table table : database.getTables()) {
+			r.add("");
+			String rTableName = convertToRName(table.getName());
+			r.add("  defaults <- list();");
+			for (Field field : table.getFields()) {
+				String rFieldName = field.getName().replaceAll(" ", "_").replaceAll("-", "_");
+				String defaultValue;
+				if (field.getValueCounts().length == 0)
+					defaultValue = "";
+				else
+					defaultValue = field.getValueCounts()[0][0];
+				if (!defaultValue.equals(""))
+					r.add("  defaults$" + rFieldName + " <- \"" + defaultValue + "\";");
+			}
+			r.add("  self$defaultValues$" + rTableName + " <- defaults;");
+		}
+		r.add("");
+		createDeclareTestFunction(r);
+		r.add("");
+		createSetDefaultFunctions(r, etl.getSourceDatabase());
+		r.add("");
+		createGetDefaultFunctions(r, etl.getSourceDatabase());
+		r.add("");
+		createAddFunctions(r, etl.getSourceDatabase());
+		r.add("");
+		createExpectFunctions(r, DEFAULT, etl.getTargetDatabase());
+		r.add("");
+		createExpectFunctions(r, NEGATE, etl.getTargetDatabase());
+		r.add("");
+		createExpectFunctions(r, COUNT, etl.getTargetDatabase());
+		r.add("  return (self);");
+		r.add("}");
+		r.add("");
+	}	
 }
